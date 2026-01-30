0e01f6ae313524131484ba8f61df2121c9ad41f4
Antash28 <antashsinha94@gmail.com>
2026-01-30 23:31:54 +0530

    initial commit

---
+++ list_operation_tute_dude_02.py
@@ -0,0 +1,39 @@
+# "extend" function in lists
+fruits = ["apple", "mango", "orange"]
+print("\n")
+print(fruits)
+print("\n")
+print(len(fruits))
+print("\n")
+fruits.extend(["Banana", "Grapes"])
+print(fruits)
+print("\n")
+print(len(fruits))
+print("\n")
+fruits.append(["Guava", "Papaya"])
+print(fruits)
+print("\n")
+print(len(fruits))
+print("\n")
+
+
+# "remove" function in lists
+sub = ["Maths", "Physics", "Geography", "Hindi"]
+print(sub)
+print("\n")
+sub.remove("Physics")
+print(sub)
+print("\n")
+
+# "pop" function in lists and different ways to use it
+sub.pop(2)
+print(sub)
+print("\n")
+
+sub.pop(-1)
+print(sub)
+print("\n")
+
+sub.pop()
+print(sub)
+print("\n")
