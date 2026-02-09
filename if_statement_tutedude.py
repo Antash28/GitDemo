f915ee4a332cecea871b33f0092d16e3f691236d
Antash28 <antashsinha94@gmail.com>
2026-02-09 21:58:58 +0530

    initial commit

---
+++ if_statement_tutedude.py
@@ -0,0 +1,19 @@
+# age = float(input("Enter your age : "))
+
+# if age>=18:
+#     print("You can vote .. !")
+
+# else:
+#     print("Not eligible to vote .. !")
+
+# print("hahah :D")
+# print("\n")
+
+# program to check odd or even
+
+num = float(input("Enter a number to check : "))
+
+if num%2 == 0:
+    print("Even Number")
+else:
+    print("Odd Number")
